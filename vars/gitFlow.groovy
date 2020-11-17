9d9c77ff0e4a2bac80fca40fccc43c484f67b158
Hedi Nasr <hnasr@ippon.fr>
2020-11-17 11:41:06 +0100

    lib refactoring

--- vars/gitFlow.groovy
+++ vars/gitFlow.groovy
@@ -1,3 +1,18 @@
+def job(String name = 'BUILD', String command = 'clean install') {
+    return {
+        stage("${name}") {
+            if (params.ACTION == name) {
+                configFileProvider([configFile(fileId: 'maven-settings',
+                                               variable: 'MAVEN_SETTINGS')]) {
+                    sshagent(['gitlab']) {
+                        sh "mvn -s \"$MAVEN_SETTINGS\" ${command}"
+                    }
+                }
+            }
+        }
+    }
+}
+
 def call() {
     pipeline {
         agent any
@@ -6,73 +21,26 @@ def call() {
             maven 'maven'
         }
 
+        environment {
+            HOTFIX_VERSION = sh(script: 'git branch -r | grep hotfix | cut -d "/" -f3', returnStdout: true).trim()
+        }
+
         stages {
-            stage('Build') {
-                when { equals expected: 'BUILD', actual: params.ACTION }
-
+            stage('GitFlow') {
                 steps {
-                    configFileProvider([configFile(fileId: 'maven-settings', variable: 'MAVEN_SETTINGS')]) {
-                        sh 'mvn -s "$MAVEN_SETTINGS" clean install'
-                    }
-                }
-            }
-
-            stage('Release Start') {
-                when { equals expected: 'RELEASE_START', actual: params.ACTION }
-
-                steps {
-                    configFileProvider([configFile(fileId: 'maven-settings', variable: 'MAVEN_SETTINGS')]) {
-                        sshagent(['gitlab']) {
-                            sh 'mvn -s "$MAVEN_SETTINGS" -B gitflow:release-start'
-                        }
-                    }
-                }
-            }
-
-            stage('Release Finish') {
-                when { equals expected: 'RELEASE_FINISH', actual: params.ACTION }
-
-                steps {
-                    configFileProvider([configFile(fileId: 'maven-settings', variable: 'MAVEN_SETTINGS')]) {
-                        sshagent(['gitlab']) {
-                            sh 'mvn -s "$MAVEN_SETTINGS" -B gitflow:release-finish'
-                        }
-                    }
-                }
-            }
-
-            stage('Hotfix Start') {
-                when { equals expected: 'HOTFIX_START', actual: params.ACTION }
-
-                steps {
-                    configFileProvider([configFile(fileId: 'maven-settings', variable: 'MAVEN_SETTINGS')]) {
-                        sshagent(['gitlab']) {
-                            sh 'mvn -s "$MAVEN_SETTINGS" -B gitflow:hotfix-start'
-                        }
-                    }
-                }
-            }
-
-            stage('Hotfix Finish') {
-                when { equals expected: 'HOTFIX_FINISH', actual: params.ACTION }
-
-                environment {
-                    HOTFIX_VERSION = sh(script: 'git branch -r | grep hotfix | cut -d "/" -f3', returnStdout: true).trim()
-                }
-
-                steps {
-                    configFileProvider([configFile(fileId: 'maven-settings', variable: 'MAVEN_SETTINGS')]) {
-                        sshagent(['gitlab']) {
-                            git branch: "hotfix/$HOTFIX_VERSION", url: "$GIT_URL", credentialsId: "gitlab"
-                            sh 'mvn -s "$MAVEN_SETTINGS" -B gitflow:hotfix-finish -DhotfixVersion=$HOTFIX_VERSION'
-                        }
+                    script {
+                        job('BUILD').call()
+                        job('RELEASE_START', '-B gitflow:release-start').call()
+                        job('RELEASE_FINISH', '-B gitflow:release-finish').call()
+                        job('HOTFIX_START', '-B gitflow:hotfix-start').call()
+                        job('HOTFIX_FINISH', '-B gitflow:hotfix-finish -DhotfixVersion=$HOTFIX_VERSION').call()
                     }
                 }
             }
         }
 
         post {
-            always {
+            success {
                 deleteDir()
                 cleanWs()
             }
